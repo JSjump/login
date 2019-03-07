9af5f1282744fbd411c5fbe1c36100e5d329bc7a
Zhuyb <919220608@qq.com>
2019-03-07 22:43:20 +0800

    前后端 框架搭建

--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from "react-redux";
+import {applyMiddleware, createStore} from "redux";
+import rootReduce from './reducers';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from "redux-devtools-extension";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    rootReduce,
+    composeWithDevTools(
+        applyMiddleware(thunk,logger)
+    )
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+    <App />
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
