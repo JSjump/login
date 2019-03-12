3702b7b5040f51de896fc15071bf42506c60c284
Zhuyb <919220608@qq.com>
2019-03-12 22:28:50 +0800

    后端验证搭建 理解webpack本地服务器，及代理解决跨域

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
 import express from 'express';
+import users from './routes/users';
+import bodyParser from 'body-parser';
+
+
 let app = express();
+app.use(bodyParser.json());
+app.use('/api/users',users);
 
 app.get('/',(req,res) => {
     res.send('hello')
