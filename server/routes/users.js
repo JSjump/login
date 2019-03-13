1bfc9fc8a73614d16b75454466bdaa5b7105163e
Zybtest <2013230787@qq.com>
2019-03-13 10:23:32 +0800

    后台验证。 lodash,validator

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,29 @@
 import express from 'express';
+import isEmpty from 'lodash/isEmpty';
+import validator from 'validator';
+
 let router = express.Router();
 
+const validateInput = (data) => {
+  let errors = {};
+  validator.isEmpty(data.username) && (errors.username = "the field is required");
+  validator.isEmpty(data.email) && (errors.email = "the field is required");
+  validator.isEmpty(data.password) && (errors.password = "the field is required");
+  validator.isEmpty(data.passwordConfirmation) && (errors.passwordConfirmation = "the field is required");
+
+  return{
+    errors,
+    isValid:isEmpty(errors)
+  }
+}
+
 router.post('/',(req,res) => {
-  console.log(req.body);
+  const {errors, isValid} = validateInput(req.body);
+
+  if(!isValid) {
+    res.status(400).json(errors);
+  }
+
 });
 
 export default router;
