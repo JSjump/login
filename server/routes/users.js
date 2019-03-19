a859a04758e38b62fb841b849c208bbbfec84580
Zhuyb <919220608@qq.com>
2019-03-19 23:38:07 +0800

    1.bookshelf 的介绍与使用 2.在数据库中保存用户注册信息

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import isEmpty from 'lodash/isEmpty';
 import validator from 'validator';
+import User from '../models/user';
+// import bcrypt from 'bcryptjs';
+const bcrypt = require('bcryptjs');
 
+
+// var bcrypt = require('bcryptjs');
 let router = express.Router();
 
 const validateInput = (data) => {
@@ -23,7 +28,14 @@ const validateInput = (data) => {
 router.post('/',(req,res) => {
     const {errors, isValid} = validateInput(req.body);
     if(isValid) {
-      res.json({success: true})
+      const {username,password,email} = req.body;
+        const salt = bcrypt.genSaltSync(10);
+      const password_digest = bcrypt.hashSync(password,salt);
+      User.forge({
+          username,password_digest,email
+      },{hasTimestamps:true}).save()
+          .then(user =>res.json({success:true}))
+          .catch(err => res.status(500).json({errors:err}))
     }else {
       res.status(400).json(errors);
     }
