2ab29ce3dc9acc06cece3886d6a0e9d0467efa0d
Zybtest <2013230787@qq.com>
2019-03-21 11:45:53 +0800

    后台响应jwt给浏览器

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import User from '../models/user';
+import jwt from 'jsonwebtoken';
+import config from '../config';
 const bcrypt = require('bcryptjs');
 
 let router = express.Router();
@@ -14,6 +16,11 @@ router.post('/', (req, res) => {
         if (user) {
             // const salt = bcrypt.genSaltSync(10);
             if (bcrypt.compareSync(password, user.get('password_digest'))) {
+                 const token = jwt.sign({
+                     id:user.get("id"),
+                     username:user.get("username")
+                 },config.jwtSecret);
+                 res.json({token})
 
             } else {
                 res.status(401).json({ errors: { form: "Invalid Credentials" } })
